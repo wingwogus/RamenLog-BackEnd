b2fd7a7b1f8f5433525099456c9f984abdd5ae0f
이재현 <wingwogus@naver.com>
2025-05-27 00:11:51 +0900

    fix: 더미 식당 추가

--- src/main/java/mjc/ramenlog/service/impl/InitDataService.java
+++ src/main/java/mjc/ramenlog/service/impl/InitDataService.java
@@ -2,9 +2,12 @@ package mjc.ramenlog.service.impl;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import mjc.ramenlog.domain.Address;
 import mjc.ramenlog.domain.Member;
+import mjc.ramenlog.domain.Restaurant;
 import mjc.ramenlog.domain.Role;
 import mjc.ramenlog.repository.MemberRepository;
+import mjc.ramenlog.repository.RestaurantRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Component;
 public class InitDataService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RestaurantRepository restaurantRepository;
 
     @PostConstruct
     public void init() {
@@ -34,6 +38,18 @@ public class InitDataService {
 
         memberRepository.save(member1);
         memberRepository.save(member2);
+
+        Restaurant restaurant = Restaurant.builder()
+                .name("맛스구")
+                .address(Address.builder()
+                        .city("서울특별시")
+                        .town("은평구")
+                        .street("응암동")
+                        .build())
+                .score(100)
+                .build();
+
+        restaurantRepository.save(restaurant);
     }
 
 }
