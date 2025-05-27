359ce81cbdcd9668f2cfac366a42f806923a0fb8
이재현 <wingwogus@naver.com>
2025-05-27 10:53:35 +0900

    fix: 등급 Repository 추가

--- src/main/java/mjc/ramenlog/domain/Grade.java
+++ src/main/java/mjc/ramenlog/domain/Grade.java
@@ -4,8 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Grade {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
