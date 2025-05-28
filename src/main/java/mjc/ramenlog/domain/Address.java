f1482ab722f4a33dbfda052b9fdaf970705ef61d
gerrmang <tjdtlghks11@naver.com>
2025-05-28 21:15:16 +0900

    restTemplate 사용하기 위해 AppConfig에 Bean으로 따로 저장

--- src/main/java/mjc/ramenlog/domain/Address.java
+++ src/main/java/mjc/ramenlog/domain/Address.java
@@ -12,15 +12,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Address {
-    private String fullAddress; // 네이버 api에서가져오는 주소는 하나의 도로명 전체 문자열.
-                                // 만약 city, town street으로 나누려면 주소 파싱 로직 필요.
+    private String fullAddress;
 }
-
-// 주소 파싱 로직
-//public Address parseAddress(String roadAddress) {
-//    String[] parts = roadAddress.split(" ", 3);
-//    String city = parts.length > 0 ? parts[0] : "";
-//    String town = parts.length > 1 ? parts[1] : "";
-//    String street = parts.length > 2 ? parts[2] : "";
-//    return new Address(city, town, street);
-//}
