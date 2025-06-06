1e15df67966d80140c966a35f537c82af32f627f
이재현 <wingwogus@naver.com>
2025-06-07 03:03:50 +0900

    fix: Grade 수정

--- src/main/java/mjc/ramenlog/repository/GradeRepository.java
+++
@@ -1,7 +0,0 @@
-package mjc.ramenlog.repository;
-
-import mjc.ramenlog.domain.Grade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GradeRepository extends JpaRepository<Grade, Long> {
-}
