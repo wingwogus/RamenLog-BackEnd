0c52506873f468350691a77bb064ed8a3997348c
이재현 <wingwogus@naver.com>
2025-06-08 01:33:32 +0900

    fix: 카카오 -> 구글로 변경, 사진 저장 로직

--- src/main/java/mjc/ramenlog/controller/ImportController.java
+++ src/main/java/mjc/ramenlog/controller/ImportController.java
@@ -2,19 +2,24 @@ package mjc.ramenlog.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import mjc.ramenlog.dto.ApiResponse;
+import mjc.ramenlog.service.impl.GooglePlaceService;
 import mjc.ramenlog.service.impl.KakaoPlaceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/import")
 @RequiredArgsConstructor
-public class KakaoImportController {
+public class ImportController {
 
     private final KakaoPlaceService kakaoPlaceService;
+    private final GooglePlaceService googlePlaceService;
 
-    @PostMapping("/api/import/kakao")
+    @PostMapping("/kakao")
     public ResponseEntity<?> importFromNaver(@RequestParam String keyword) {
         try{
             kakaoPlaceService.searchAndSaveRamenShops(keyword);
@@ -22,5 +27,12 @@ public class KakaoImportController {
         }catch (Exception e){
             return ResponseEntity.status(500).body("에러발생 : " + e.getMessage());
         }
+    }
+
+    @PostMapping("/google")
+    public ResponseEntity<ApiResponse<Void>> findAndSaveRestaurant() {
+        googlePlaceService.findRestaurantAndSave();
+
+        return ResponseEntity.ok(ApiResponse.success("저장 완료", null));
     }
 }
