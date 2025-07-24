f5541acfb4a495995a2304825327d87a12c4fde1
이재현 <wingwogus@naver.com>
2025-07-24 09:59:53 +0900

    fix: 일단 초기 create

--- src/main/java/mjc/ramenlog/config/SecurityConfig.java
+++ src/main/java/mjc/ramenlog/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/",
                                 "/api/auth/**" ,
                                 "/api/import/**",
                                 "/v3/api-docs/**",
