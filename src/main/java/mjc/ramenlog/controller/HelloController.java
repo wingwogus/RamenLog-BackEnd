b858d53321e0649b41c18650fdbaa53e4130525b
이재현 <wingwogus@naver.com>
2025-07-24 10:11:50 +0900

    fix: ssl 인증서용 엔드포인트 응답 수정

--- src/main/java/mjc/ramenlog/controller/HelloController.java
+++ src/main/java/mjc/ramenlog/controller/HelloController.java
@@ -2,16 +2,20 @@ package mjc.ramenlog.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 public class HelloController {
-
     @GetMapping
-    public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello World");
+    public ResponseEntity<Map<String, Object>> hello() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("responsetime", System.currentTimeMillis());
+        response.put("message", "Hello World");
+        return ResponseEntity.ok(response);
     }
 }
